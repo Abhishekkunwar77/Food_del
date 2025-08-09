76e0ae9898088741133c0a64a2c806f6d2f889f7
Your Name <you@example.com>
2025-08-09 10:04:47 +0530

    in sorted order

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -91,7 +91,7 @@ const userOrders = async (req, res) => {
 // listing orders for admin panel 
 const listOrders = async (req, res) => {
   try {
-    const orders= await orderModel.find({});
+const orders = await orderModel.find({}).sort({ date: -1 });
     res.json({success:true, data:orders})
     
   } catch (error) {
